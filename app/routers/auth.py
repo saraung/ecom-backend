8cc6807dd51199f90d1429f1a5472259649be01a
saraung <saraungbabu@gmail.com>
2026-02-23 20:59:49 +0530

    auth rout

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,2 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.schemas.user import UserCreate, UserResponse
-from app.schemas.auth import LoginRequest, TokenResponse
